50e61616e1ca1ccd151768ef8ccfb91e0c0d2ad3
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2022-01-07 22:51:15 +0900

    Add files via upload

--- coax_control/include/coax_control/coax_control.h
+++ coax_control/include/coax_control/coax_control.h
@@ -10,13 +10,39 @@
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
+using nav_msgs::Odometry;
+
 using Eigen::Vector3d;
 using Eigen::Matrix3d;
 
 class CoaxCTRL{
     public:
+    // Constructor
+    CoaxCTRL();
+
+    // Callback trajectory
+
+
+    // Callback Pose
+    void CallbackPose(const Odometry & pose_msg);
+
+    // Position Controller
+    void PosControl();
+
+    // Orientation Controller
+    void OriControl();
+
+    // Destructor
+    ~CoaxCTRL();
 
     private:
+    ros::NodeHandle nh;
+    ros::Subscriber traj_subscriber;
+    ros::Subscriber pose_subscriber;
+    ros::Publisher throttle_publisher;
+    ros::Publisher tvc_publisher;
+    ros::Publisher rudder_publisher;
+
     Vector3d I_p_CM;
     Vector3d I_v_CM;
     Vector3d I_a_CM;
@@ -25,12 +51,17 @@ class CoaxCTRL{
     Vector3d I_v_des;
     Vector3d I_a_des;
 
+    Vector3d CM_p_CM2T;
+
     double Kp_pos;
     double Kd_pos;
 
     double Kp_ori;
     double Kd_ori;
 
+    double throttle;
+    double des_rp;
+    double des_yaw;
 };
 
 #endif
