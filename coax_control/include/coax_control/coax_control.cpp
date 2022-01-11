392734553461a2b965a2235d65c596f511fcdcdf
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2022-01-11 16:29:54 +0900

    Add files via upload

---
+++ coax_control/include/coax_control/coax_control.cpp
@@ -0,0 +1,80 @@
+#include "coax_control.h"
+
+CoaxCTRL::CoaxCTRL()
+{
+    // Constructor
+    cout<<"Mass Parameter Setup"<<endl;
+    nh.getParam("mass",mass);
+    I_g_CM << 0, 0, mass*g;
+
+    cout<<"COM Offset Parameter Setup"<<endl;
+    nh.getParam("x_com_off",x_com_off);
+    nh.getParam("y_com_off",y_com_off);
+    nh.getParam("z_com_off",z_com_off);
+
+    cout<<"***Get Position Gain Parameter***"<<endl;
+    
+    nh.getParam("Kp_pos",Kp_pos);
+    nh.getParam("Kd_pos",Kd_pos);
+
+    cout<<"Kp_pos Gain: "<<Kp_pos<<endl;
+    cout<<"Kd_pos Gain: "<<Kd_pos<<endl;
+
+    nh.getParam("Kp_ori",Kp_ori);
+    nh.getParam("Kd_ori",Kd_ori);
+
+    cout<<"Kp_ori Gain: "<<Kp_ori<<endl;
+    cout<<"Kd_ori Gain: "<<Kd_ori<<endl;
+
+    nh.getParam("C_lift",C_lift);
+    
+    cout<<"***Get Lift Parameter***"<<endl;
+
+    cout<<"C lift : "<<C_lift<<endl;
+
+    cout<<"***Subscriber Setup***"<<endl;
+
+    cout<<"Desired Trajectory Subscriber Setup"<<endl;
+
+    cout<<"Estimated Pose Subscriber Setup"<<endl;
+    pose_subscriber = nh.subscribe("/mavros/local_position/odom",1,&CoaxCTRL::CallbackPose,this);
+
+    cout<<"Throttle Publisher Setup"<<endl;
+    throttle_publisher = nh.advertise<Int32>("/throttle",1);
+    
+    cout<<"TVC roll pitch Publisher Setup"<<endl;
+    tvc_publisher = nh.advertise<lm4075e_msgs::Int32>("/des_rp",1);
+
+    cout<<"Rudder Publisher Setup"<<endl;
+    rudder_publisher = nh.advertise<Int32>("/des_yaw",1);
+
+
+}
+
+void CoaxCTRL::CallbackDesPos(const Pose & des_p)
+{
+    I_p_des << des_p.position.x, 
+            des_p.position.y, 
+            des_p.position.z;
+}
+
+void CoaxCTRL::CallbackDesVel(const Twist & des_v)
+{
+    I_v_des << des_v.linear.x,
+            des_v.linear.y,
+            des_v.linear.z;
+}
+
+void CoaxCTRL::CallbackDesAcc(const Accel & des_a)
+{
+    I_a_des << des_a.linear.x,
+            des_a.linear.y,
+            des_a.linear.z;
+}
+
+void CoaxCTRL::PosControl()
+{
+    u_pos = I_a_des 
+            + Kp_pos*(I_p_des - I_p_CM) 
+            + Kd_pos*(I_v_des - I_v_CM);
+}
--- coax_control/include/coax_control/coax_control.h
+++ coax_control/include/coax_control/coax_control.h
@@ -4,16 +4,28 @@
 #include <ros/ros.h>
 #include <iostream>
 
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Accel.h>
+#include <geometry_msgs/Twist.h>
+
 #include <nav_msgs/Odometry.h>
+#include <std_msgs/Int32.h>
 #include <lm4075e_msgs/Int32.h>
 
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
+using geometry_msgs::Pose;
+using geometry_msgs::Twist;
+using geometry_msgs::Accel;
 using nav_msgs::Odometry;
+using std_msgs::Int32;
 
 using Eigen::Vector3d;
 using Eigen::Matrix3d;
+
+using std::cout;
+using std::endl;
 
 class CoaxCTRL{
     public:
@@ -21,9 +33,11 @@ class CoaxCTRL{
     CoaxCTRL();
 
     // Callback trajectory
+    void CallbackDesPos(const Pose & des_p);
+    void CallbackDesVel(const Twist & des_v);
+    void CallbackDesAcc(const Accel & des_a);
 
-
-    // Callback Pose
+    // Callback Estimated Pose
     void CallbackPose(const Odometry & pose_msg);
 
     // Position Controller
@@ -46,18 +60,29 @@ class CoaxCTRL{
     Vector3d I_p_CM;
     Vector3d I_v_CM;
     Vector3d I_a_CM;
+    Vector3d I_g_CM;
     
     Vector3d I_p_des;
     Vector3d I_v_des;
     Vector3d I_a_des;
 
+    Vector3d u_pos;
+
     Vector3d CM_p_CM2T;
+
+    double mass;
+    const double g = -9.81;
+    double x_com_off;
+    double y_com_off;
+    double z_com_off;
 
     double Kp_pos;
     double Kd_pos;
 
     double Kp_ori;
     double Kd_ori;
+
+    double C_lift;
 
     double throttle;
     double des_rp;
